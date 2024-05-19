2713f08d86ea279b1159110083ca1a4004390f0f
Alejo <alejoavenali@gmail.com>
2024-05-19 17:12:13 -0300

    add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import PostsList from "./components/PostsList"
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+        <h1>React on Rails blog</h1>
+        <PostsList />
+      </div>
+    </>
+  )
+}
+
+export default App
